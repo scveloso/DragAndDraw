a46bfe1d3bdb3016481f6cb71f8e26c67dbeb8dc
Samuel Christian Veloso <samtveloso@gmail.com>
2016-06-13 16:15:37 -0700

    Added option to create lines rather than boxes through the toolbar

--- app/src/main/java/com/bignerdranch/android/draganddraw/BoxDrawingView.java
+++ app/src/main/java/com/bignerdranch/android/draganddraw/BoxDrawingView.java
@@ -17,12 +17,15 @@ import java.util.List;
  */
 public class BoxDrawingView extends View{
 
-    private static final String TAG = "BoxDrawingView";
-
     private Box mCurrentBox;
     private List<Box> mBoxen = new ArrayList<>();
+
+    private Line mCurrentLine;
+    private List<Line> mLines = new ArrayList<>();
+
     private Paint mBoxPaint;
     private Paint mBackgroundPaint;
+    private String mMode;
 
     // Used when creating the view in code
     public BoxDrawingView(Context context) {
@@ -40,25 +43,35 @@ public class BoxDrawingView extends View{
         // Paint the background off-white
         mBackgroundPaint = new Paint();
         mBackgroundPaint.setColor(0xfff8efe0);
+
+        mMode = "Box";
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
+        switch (mMode) {
+            case "Line":
+                handleLineEvent(event);
+                break;
+            case "Box":
+                handleBoxEvent(event);
+                break;
+        }
+        return true;
+    }
+
+    private void handleBoxEvent(MotionEvent event) {
         PointF current = new PointF(event.getX(), event.getY());
-        String action = "";
 
         switch(event.getAction()) {
             case MotionEvent.ACTION_DOWN:
-                action = "ACTION_DOWN";
                 mCurrentBox = new Box(current);
                 mBoxen.add(mCurrentBox);
                 break;
             case MotionEvent.ACTION_UP:
-                action = "ACTION_UP";
                 mCurrentBox = null;
                 break;
             case MotionEvent.ACTION_MOVE:
-                action = "ACTION_MOVE";
                 if (mCurrentBox != null) {
                     mCurrentBox.setCurrent(current);
                     // Makes the BoxDrawingView invalid again, causing it to redraw itself and its children - calling onDraw
@@ -66,14 +79,33 @@ public class BoxDrawingView extends View{
                 }
                 break;
             case MotionEvent.ACTION_CANCEL:
-                action = "ACTION_CANCEL";
                 mCurrentBox = null;
                 break;
         }
+    }
 
-        Log.i(TAG, action + " at x =" + current.x + ", y=" + current.y);
+    private void handleLineEvent (MotionEvent event) {
+        PointF current = new PointF(event.getX(), event.getY());
 
-        return true;
+        switch(event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                mCurrentLine = new Line(current);
+                mLines.add(mCurrentLine);
+                break;
+            case MotionEvent.ACTION_UP:
+                mCurrentLine = null;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if (mCurrentLine != null) {
+                    mCurrentLine.setCurrent(current);
+                    // Makes the BoxDrawingView invalid again, causing it to redraw itself and its children - calling onDraw
+                    invalidate();
+                }
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                mCurrentLine = null;
+                break;
+        }
     }
 
     @Override
@@ -89,5 +121,18 @@ public class BoxDrawingView extends View{
 
             canvas.drawRect(left, top, right, bottom, mBoxPaint);
         }
+
+        for (Line line: mLines) {
+            float startX = line.getOrigin().x;
+            float startY = line.getOrigin().y;
+            float endX = line.getCurrent().x;
+            float endY = line.getCurrent().y;
+
+            canvas.drawLine(startX, startY, endX, endY, mBoxPaint);
+        }
+    }
+
+    public void setMode (String s) {
+        mMode = s;
     }
 }
--- app/src/main/java/com/bignerdranch/android/draganddraw/DragAndDrawFragment.java
+++ app/src/main/java/com/bignerdranch/android/draganddraw/DragAndDrawFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.Layout;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -11,6 +14,8 @@ import android.view.ViewGroup;
  * Created by Veloso on 6/13/2016.
  */
 public class DragAndDrawFragment extends Fragment {
+
+    private BoxDrawingView mDrawingView;
 
     public static DragAndDrawFragment newInstance() {
         return new DragAndDrawFragment();
@@ -20,6 +25,34 @@ public class DragAndDrawFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_drag_and_draw, container, false);
 
+        mDrawingView = (BoxDrawingView) v.findViewById(R.id.box_drawing_view);
+
         return v;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_drag_and_draw, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.menu_item_box_mode:
+                mDrawingView.setMode("Box");
+                return true;
+            case R.id.menu_item_line_mode:
+                mDrawingView.setMode("Line");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
